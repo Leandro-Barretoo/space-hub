140810bd670cd329f27b31eaa9b42725ad08a590
Abdulrahman <boodynaser@rocketmail.com>
2021-09-06 22:25:35 +0200

    Style Rock component

--- src/Components/Rocketcomponent.js
+++ src/Components/Rocketcomponent.js
@@ -1,16 +1,19 @@
+/* eslint-disable no-unused-vars */
 import PropTypes from 'prop-types';
 import Button from './Rockets_Button';
+import logo from '../Assets/rocket.jpg';
+import style from './Rocketcomponent.module.css';
 
 const Rocket = (props) => {
   const {
     imgsrc, imgname, rocketname, rocketdescription,
   } = props;
   return (
-    <div className="rocketcontaine">
-      <div className="imgcontainer">
-        <img src={imgsrc} alt={imgname} />
+    <div className={style.rocketcontainer}>
+      <div className={style.imgcontainer}>
+        <img src={logo} alt={imgname} className={style.img} />
       </div>
-      <div className="descriptioncontainer">
+      <div className={style.descriptioncontainer}>
         <h1>{rocketname}</h1>
         <p>{rocketdescription}</p>
         <Button reserved={false} />
--- src/Components/Rockets.js
+++ src/Components/Rockets.js
@@ -2,7 +2,7 @@ import Rocket from './Rocketcomponent';
 
 const Rockets = () => (
   <>
-    <Rocket imgsrc="dsfsdf" imgname="rock" rocketname="rocket usa" rocketdescription="this rocket is one of the most blalablablab" />
+    <Rocket imgsrc="dsfsdf" imgname="rock" rocketname="Falcon 1" rocketdescription="The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 2 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth." />
   </>
 );
 
