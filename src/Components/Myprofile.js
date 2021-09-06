8de05c5f0888c9d1ef4903d35ec843250cc63c1c
Abdulrahman <boodynaser@rocketmail.com>
2021-09-06 12:46:28 +0200

    Create Myprofile component

---
+++ src/Components/Myprofile.js
@@ -0,0 +1,9 @@
+const Myprofile = () => (
+  <div className="queryresult">
+    {/* Selected Missions */}
+    <div />
+    {/* Selected Rockets */}
+    <div />
+  </div>
+);
+export default Myprofile;
