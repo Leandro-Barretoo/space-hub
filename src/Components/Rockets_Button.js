3f4b704841d3a8275311ffd858451d45ba90b149
Abdulrahman <boodynaser@rocketmail.com>
2021-09-06 22:35:22 +0200

    Style Button component

--- src/Components/Rockets_Button.js
+++ src/Components/Rockets_Button.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
+import stylebutt from './Rockets_Button.module.css';
 
 const Button = (props) => {
   const reserved = props;
   if (reserved) {
-    return <button type="button">Reserve</button>;
+    return <button className={stylebutt.buttRes} type="button">Reserve Rocket</button>;
   }
-  return <button type="button">Cancel Reservation</button>;
+  return <button className={stylebutt.buttRes} type="button">Cancel Reservation</button>;
 };
 
 Button.proptype = {
