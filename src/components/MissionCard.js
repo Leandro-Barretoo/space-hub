8ef37099e16019d65dd978859dddb8bee839970c
Leandro <officialdevmi@gmail.com>
2021-09-07 09:14:36 -0700

    Create click event to dispatch join action

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,14 +1,31 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission } from '../redux/missions/missions';
 
 const MissionCard = (props) => {
-  const { name, description } = props;
+  const { name, description, missionid } = props;
+  const dispatch = useDispatch();
+
+  const joinMissionHandler = () => {
+    dispatch(joinMission(missionid));
+  };
 
   return (
     <tr>
       <td className="Table-Content Mission">{name}</td>
       <td className="Table-Content Description Desc-Info">{description}</td>
-      <td className="Table-Content Status Btn"><span className="Member">NOT A MEMBER</span></td>
-      <td className="Table-Content Empty Btn"><button type="submit" className="Join">Join Mission</button></td>
+      <td className="Table-Content Status Btn">
+        <span className="Member">NOT A MEMBER</span>
+      </td>
+      <td className="Table-Content Empty Btn">
+        <button
+          type="submit"
+          className="Join"
+          onClick={joinMissionHandler}
+        >
+          Join Mission
+        </button>
+      </td>
     </tr>
   );
 };
@@ -16,11 +33,13 @@ const MissionCard = (props) => {
 MissionCard.defaultProps = {
   name: '',
   description: '',
+  missionid: '',
 };
 
 MissionCard.propTypes = {
   name: PropTypes.string,
   description: PropTypes.string,
+  missionid: PropTypes.string,
 };
 
 export default MissionCard;
