e90070a410f53b59884fa2114b3ae0b62381d24b
Abdulrahman <boodynaser@rocketmail.com>
2021-09-07 16:23:14 +0200

    Remove unused Local component's State

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,20 +1,16 @@
-/* eslint-disable max-len */
-/* eslint-disable no-unused-vars */
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Rocket from './Rocketcomponent';
 import fetchrockets from '../utils/rockets_API';
 import { addrocks } from '../redux/rockets/rocks';
 
 const Rockets = () => {
-  const [rockets, setRockets] = useState([]);
   const stat = useSelector((state) => state.rocksReducer);
   const dipsatch = useDispatch();
   const loadRockets = async () => {
     const rocktObj = await fetchrockets();
     if (stat.length !== rocktObj.length) {
       dipsatch(addrocks(rocktObj));
-      setRockets(rocktObj);
     }
   };
 
