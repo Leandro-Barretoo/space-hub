72b21ab1235d4237d11576410c9ba6a92b3421e9
Leandro <officialdevmi@gmail.com>
2021-09-08 01:36:02 -0700

    Add card element and table styles

--- src/components/Myprofile.js
+++ src/components/Myprofile.js
@@ -1,9 +1,15 @@
 import './Myprofile.css';
+import ProfileMission from './ProfileMission';
 
 const Myprofile = () => (
   <div className="queryresult">
     <div className="Box">
       <h2 className="Cat-Title">My Missions</h2>
+      <table className="Mission-ProfileTable">
+        <tbody>
+          <ProfileMission />
+        </tbody>
+      </table>
     </div>
     <div className="Box">
       <h2 className="Cat-Title">My Rockets</h2>
---
+++ src/components/ProfileMission.js
@@ -0,0 +1,7 @@
+const ProfileMission = () => (
+  <tr>
+    <td className="Title-Entrie">Hello</td>
+  </tr>
+);
+
+export default ProfileMission;
