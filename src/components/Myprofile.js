9e1defefb7bbfbbf2c500d9360f681988b03164d
Leandro <officialdevmi@gmail.com>
2021-09-08 02:04:39 -0700

    Add function to render reserved missions

--- src/components/Myprofile.js
+++ src/components/Myprofile.js
@@ -1,19 +1,30 @@
+import { useSelector } from 'react-redux';
 import './Myprofile.css';
 import ProfileMission from './ProfileMission';
 
-const Myprofile = () => (
-  <div className="queryresult">
-    <div className="Box">
-      <h2 className="Cat-Title">My Missions</h2>
-      <table className="Mission-ProfileTable">
-        <tbody>
-          <ProfileMission />
-        </tbody>
-      </table>
+const Myprofile = () => {
+  const missions = useSelector((state) => state.missionsReducer);
+
+  const reservedMissions = missions.filter((mission) => mission.reserved === true);
+
+  const renderList = reservedMissions.map((mission) => (
+    <ProfileMission key={mission.mission_id} content={mission.mission_name} />
+  ));
+
+  return (
+    <div className="queryresult">
+      <div className="Box">
+        <h2 className="Cat-Title">My Missions</h2>
+        <table className="Mission-ProfileTable">
+          <tbody>
+            {renderList}
+          </tbody>
+        </table>
+      </div>
+      <div className="Box">
+        <h2 className="Cat-Title">My Rockets</h2>
+      </div>
     </div>
-    <div className="Box">
-      <h2 className="Cat-Title">My Rockets</h2>
-    </div>
-  </div>
-);
+  );
+};
 export default Myprofile;
--- src/components/ProfileMission.js
+++ src/components/ProfileMission.js
@@ -1,7 +1,20 @@
-const ProfileMission = () => (
-  <tr>
-    <td className="Title-Entrie">Hello</td>
-  </tr>
-);
+import PropTypes from 'prop-types';
+
+const ProfileMission = (props) => {
+  const { content } = props;
+  return (
+    <tr>
+      <td className="Title-Entrie">{content}</td>
+    </tr>
+  );
+};
+
+ProfileMission.defaultProps = {
+  content: '',
+};
+
+ProfileMission.propTypes = {
+  content: PropTypes.string,
+};
 
 export default ProfileMission;
